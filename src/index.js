a33d29a8fc0bb52fb1b13edb770d2b867b6836f5
Maksym-1989 <zaksumy1989@gmail.com>
2021-05-12 12:08:19 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.css';
+import './js/test.js';
+// import printText from './js/test'; дефолтный импорт
+import { printText } from './js/test';
+printText(1222225555);
---
+++ src/js/test.js
@@ -0,0 +1,11 @@
+import menu from '../menu.json';
+console.log(menu);
+
+export const a = 'hello max';
+
+export function printText(string) {
+  console.log(string);
+}
+
+printText(a);
+// export default printText;
